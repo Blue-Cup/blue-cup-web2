3f2c9eb0e8d10493e747024d495e472694e5d650
Matthew Hirth <hirth.24@osu.edu>
2022-03-07 19:22:35 -0500

    removed logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home/Home";
@@ -62,3 +61,5 @@ function App() {
 }
 
 export default App;
+
+
